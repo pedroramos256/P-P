481f29a5706c281d6f8fdc34d7307cbcdfe95dc2
sanchabbarroso <spbbbarroso@gmail.com>
2022-10-17 15:14:52 +0100

    added distances matrix to proj.py

--- proj.py
+++ proj.py
@@ -47,11 +47,17 @@ ix = 0
 while example_scenario[ix][0] == "#":
     ix += 1
 A = int(example_scenario[ix])
+start = []
+end = []
+for i in range(A):
+    start.append(0)
+    end.append(0)
 str_out += f"A={A};\nstart=["
 for l in example_scenario[ix+2:ix+2+A]:
     i,j = l.split(' ')
     i = int(i)
     j = int(j)
+    start[i-1] = j
     str_out += f"{j}"
     if i < A:
         str_out += ","
@@ -61,13 +67,48 @@ for l in example_scenario[ix+2+A+1:]:
     i,j = l.split(' ')
     i = int(i)
     j = int(j)
+    end[i-1] = j
     str_out += f"{j}"
     if i < A:
         str_out += ","
 
 str_out += "];\n"
 
-for t in tqdm(range(2,30)):
+print(matrix)
+print(start)
+print(end)
+
+distances = np.zeros((V,V))
+
+def bfs(visited, graph, node, queue): #function for BFS
+    visited.append(node)
+    queue.append([node, 0])
+    while queue:          # Creating loop to visit each node
+        m, distance = queue.pop(0)
+        distances[node][m] = distance 
+        for neighbour in graph[m]:
+            if (neighbour-1) not in visited:
+                queue.append([neighbour-1, distance+1])
+                visited.append(neighbour-1)
+
+for v in tqdm(range(V)):
+    visited = []    # List for visited nodes.
+    queue = []      #Initialize a queue
+    bfs(visited, matrix, v, queue)
+print("Distâncias")
+print(distances)
+
+t_lower = 0
+for i in range(len(start)):
+    dist = distances[start[i]-1][end[i]-1]
+    if dist > t_lower:
+        t_lower = dist
+
+print(t_lower)
+
+exit(0)
+
+for t in tqdm(range(t_lower,30)):
     str_out_w_t = f"T={t};\n"+str_out
     file = open(f"dzn/{prefix}-input.dzn", "w")
     file.write(str_out_w_t)
