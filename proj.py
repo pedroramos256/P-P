42f202f64ed7fd01435e2d6681f51b153c15c246
pedroramos256 <pedrojgcramos@gmail.com>
2022-10-21 16:53:20 +0200

    best version so far

--- proj.py
+++ proj.py
@@ -7,8 +7,13 @@ from tqdm import tqdm
 filename_graph = sys.argv[1]
 filename_scenario = sys.argv[2]
 
-example_graph = open(f'{filename_graph}').readlines()
-example_scenario = open(f'{filename_scenario}').readlines()
+f_graph = open(f'{filename_graph}')
+example_graph = f_graph.readlines()
+f_graph.close()
+
+f_scen = open(f'{filename_scenario}')
+example_scenario = f_scen.readlines()
+f_scen.close()
 prefix = filename_graph.split('/')[1].split('-')[0]
 
 ix = 0
@@ -25,13 +30,14 @@ for l in example_graph[ix+2:]:
     j = int(j)-1
     graph[i].append(j+1)
     graph[j].append(i+1)
+
 #for i in range(V):
 #    graph[i].append(i+1)
 
 
-def print_graph(graph):
+def print_graph(graph,new_V):
     str_out="graph=["
-    for i in range(V):
+    for i in range(new_V):
         str_out += "{"
         for j in range(len(graph[i])):
             str_out += str(graph[i][j])
@@ -40,7 +46,7 @@ def print_graph(graph):
         
         str_out += "}"
 
-        if i < V-1:
+        if i < new_V-1:
             str_out += ","
         
     str_out += "];\n"
@@ -105,18 +111,18 @@ for v in range(V):
     distances = bfs(visited, graph, v, queue, distances)
 
 
-def print_distances(dist):
+def print_distances(dist,new_V):
     str_out = "distances=[|"
-    for i in tqdm(range(V)):
-        for j in range(V):
+    for i in range(new_V):
+        for j in range(new_V):
             str_out += f"{int(dist[i][j])}"
         
-            if j < V-1:
+            if j < new_V-1:
                 str_out += ","
-        if i < V-1:
+        if i < new_V-1:
             str_out += "\n|"
-        else:
-            str_out += "|];\n"
+        
+    str_out += "|];\n"
     return str_out
 
 t_lower = 0
@@ -125,104 +131,105 @@ for i in range(len(start)):
     if dist > t_lower:
         t_lower = dist
 
-print(sum(distances)/(V-1))
-exit()
+
 
 for t in tqdm(range(int(t_lower+1),30)):
+    
+    
     v_to_delete = []
     for v in range(V):
-        best_time = t
-        entered = False
-        for a in range(A):
-            if start[a]-1 != v and end[a]-1 != v:
-                entered = True
+        if v+1 not in start and v+1 not in end:
+            best_time = t
+            for a in range(A):
+                #if counter == 1: print(len(distances),v,start[a]-1,end[a]-1)
                 time_for_a = distances[start[a]-1][v] + distances[v][end[a]-1]
+                #print("v",v,start[a]-1,end[a]-1,time_for_a)
                 if time_for_a < best_time:
                     best_time = time_for_a
-        if best_time >= t and entered:
-            v_to_delete.append(v)
-    #print(v_to_delete)
+            
+            if best_time >= t:
+                v_to_delete.append(v)
+
+    #print("v_to_delete",len(v_to_delete))
     new_distances = distances
     new_distances = np.delete(new_distances, v_to_delete, axis=0)
     new_distances = np.delete(new_distances, v_to_delete, axis=1)
     #print(new_distances)
     new_graph = []
-    mapping = np.zeros((V))
 
-    V = len(new_distances)
+    new_V = len(new_distances)
     #print(V)
     
-    for i in range(V):
+    for i in range(new_V):
         new_connections = []
-        for j in range(V):
+        for j in range(new_V):
             if new_distances[i][j] == 1:
                 new_connections.append(j+1)
         new_graph.append(new_connections)
 
-    new_distances = np.zeros((V,V))
-    for v in range(V):
+    new_distances = np.zeros((new_V,new_V))
+    for v in range(new_V):
         visited = []    # List for visited nodes.
         queue = []      #Initialize a queue
         new_distances = bfs(visited, new_graph, v, queue, new_distances)
-    
+    #print(new_distances)
     #print(distances)
     #print("\n")
     #print(new_graph)
     #print("\n")
     #print(V,len(new_graph), len(distances))
     #print(v_to_delete)
+    mapping = [0]*V
+    #print("start_before",start)
+    new_start = np.zeros((A),dtype=int)
+    new_end = np.zeros((A),dtype=int)
     if v_to_delete != []:
-        for i in range(len(mapping)):
+        for i in range(V):
             for c,v in enumerate(v_to_delete):
-                if v > i:
+                if v == i:
+                    mapping[i] = 0
+                    break
+                elif v > i:
                     mapping[i] = i+1-c
                     break
             if v_to_delete[-1] < i:
                 mapping[i] = i+1-len(v_to_delete)
 
         for i in range(A):
-            start[i] = mapping[start[i]-1]
-            end[i] = mapping[end[i]-1]
+            new_start[i] = mapping[start[i]-1]
+            new_end[i] = mapping[end[i]-1]
+    else:
+        for i in range(V):
+            mapping[i] = i+1
+        new_start = start
+        new_end = end
+
+    #print("mapping",mapping)
+    #print("start_after",start)
     
-    print(mapping)
 
-    """
-        for i in range(A):
-            for c,v in enumerate(v_to_delete):
-                if v > start[i]-1:
-                    start[i] -= c
-                    break
-            if v_to_delete[-1] < start[i]-1:
-                start[i] -= len(v_to_delete)
 
-            for c,v in enumerate(v_to_delete):
-                if v > end[i]-1:
-                    end[i] -= c
-                    break
-            if v_to_delete[-1] < end[i]-1:
-                end[i] -= len(v_to_delete)
-    """
-
-    str_out_w_t = f"T={t};\nV={V};\n"+print_graph(new_graph)+print_start_end(start,end)+print_distances(new_distances)
+    str_out_w_t = f"T={t};\nV={new_V};\n"+print_graph(new_graph,new_V)+print_start_end(new_start,new_end)+print_distances(new_distances,new_V)
     #print(str_out_w_t)
     file = open(f"dzn/{prefix}-input.dzn", "w")
     file.write(str_out_w_t)
     file.close()
-    os.system(f"minizinc --solver chuffed solver3.mzn dzn/{prefix}-input.dzn > mzn/{prefix}-solution.txt")
+    #print("entering minizinc")
+    os.system(f"minizinc --solver chuffed solver2.mzn dzn/{prefix}-input.dzn > mzn/{prefix}-solution.txt")
     
-    lines = open(f"mzn/{prefix}-solution.txt","r").readlines()
+    f_sol = open(f"mzn/{prefix}-solution.txt","r")
+    lines = f_sol.readlines()
+    f_sol.close()
     if len(lines) > 1:
         output = ""
         for i,l in enumerate(lines[:-1]):
             nodes = l.split()
 
-            nodes_after = [4,2,3]
-            v_to_delete = [2,3,4,6]
-            nodes_before = [8,5,7]
             for j in range(len(nodes)):
-                nodes[j] = np.where(mapping==int(nodes[j])+1)  
-            print(nodes)
-            exit(0)
+                #print(mapping)
+                #print(nodes[j])
+                nodes[j] = mapping.index(int(nodes[j]))+1
+            #print(nodes)
             output += f"i={i}\t"
             for j,node in enumerate(nodes):
                 output += f"{j+1}:{node}"
--- solve_all.py
+++ solve_all.py
@@ -6,6 +6,6 @@ scens = os.listdir('scen')
 graphs.sort()
 scens.sort()
 
-for i in tqdm(range(len(graphs))):
+for i in range(len(graphs)):
     print(graphs[i],scens[i])
     os.system(f'python3 proj.py graph/{graphs[i]} scen/{scens[i]} > sol/ex0{i+1}-sol.txt')
